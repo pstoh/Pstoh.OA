ef0b18ef43aef994a38891b5b0962ca08a658f4d
pstoh <pstoh122655@qq.com>
2019-04-19 10:59:29 +0800

    实现异常过滤

--- Pstoh.OA.UI.Portal/App_Start/FilterConfig.cs
+++ Pstoh.OA.UI.Portal/App_Start/FilterConfig.cs
@@ -7,7 +7,8 @@ namespace Pstoh.OA.UI.Portal
 	{
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
-			filters.Add(new HandleErrorAttribute());
+			//filters.Add(new HandleErrorAttribute());
+			filters.Add(new UI.Portal.Models.MyExceptionFilterAttribute());
 		}
 	}
 }
